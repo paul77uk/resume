187076d7220cb8379ac58ac94e003b79771c3973
paul77uk <paul77uk@gmail.com>
2024-08-11 03:27:26 +0100

    corrected link and added project

--- components/RightColumn.tsx
+++ components/RightColumn.tsx
@@ -29,19 +29,10 @@ const RightColumn = () => {
       <DetailSection
         title="Built4Life"
         dateText="05/2024 - present"
-        link="https://built4life"
+        link="https://built4.life"
         text={[
           "A workout app that allows users to create workouts and track their progress",
           "Technologies used: Next.js, TypeScript, Drizzle ORM, Shadcn UI, Tailwind CSS, Vercel.",
-        ]}
-      />
-      <DetailSection
-        title="Trailblazr"
-        school="Le Wagon"
-        dateText="09/2023"
-        link="http://www.trailblazr.me/"
-        text={[
-          "Designed, Implemented and shipped to production our own web app Trailblazr and completed the project in 2 weeks in a team of 5",
         ]}
       />
       <DetailSection
@@ -52,6 +43,27 @@ const RightColumn = () => {
           "I built this resume using TypeScript, Next.js, React and Tailwind CSS and deployed it on Vercel.",
         ]}
       />
+      <DetailSection
+        title="AI Chatbot"
+        dateText="08/2024"
+        link="https://ai-chat-eight-pearl.vercel.app/"
+        text={[
+          "I built this AI Chat App using TypeScript, Next.js, React, Kinde, Prisma, Zod, Tailwind, Shadcn and deployed it on Vercel.",
+          "It has auth with Kinde, email and google login, full crud to save responses, and then capability to edit and delete them.",
+        ]}
+      />
+
+      {/* <div className="pt-10"> */}
+        <DetailSection
+          title="Trailblazr"
+          school="Le Wagon"
+          dateText="09/2023"
+          link="http://www.trailblazr.me/"
+          text={[
+            "Designed, Implemented and shipped to production our own web app Trailblazr and completed the project in 2 weeks in a team of 5",
+          ]}
+        />
+      {/* </div> */}
     </main>
   );
 };
